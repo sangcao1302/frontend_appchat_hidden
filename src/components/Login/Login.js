94076ec9adbadfe5d65e2b020be305bc6527ece2
sangcao1302 <caochisang1302@gmail.com>
2025-02-24 13:21:44 +0700

    update button login

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -213,19 +213,194 @@
 
 
 
+// import React, { useState, useEffect } from 'react';
+// import axios from 'axios';
+// import { useNavigate } from 'react-router-dom';
+// import { io } from 'socket.io-client';
+// import { GoogleLogin } from '@react-oauth/google';
+// import jwtDecode from 'jwt-decode';
+// // import getLocation from '../../utils/getLocation';
+// import "./Login.css";
+// const baseURL = process.env.REACT_APP_BASE_URL;
+
+// const socket = io(baseURL); // Initialize the socket connection
+
+
+
+// const Login = ({ onLogin }) => {
+//     const [gender, setGender] = useState('');
+//     const [age, setAge] = useState('');
+//     const [location, setLocation] = useState('');
+//     const [errorMessage, setErrorMessage] = useState('');
+//     const [googleUser, setGoogleUser] = useState(null);
+//     const navigate = useNavigate();
+
+//     useEffect(() => {
+//         // Check if a token already exists in local storage
+//         const token = localStorage.getItem('token');
+//         const userId = localStorage.getItem('userId');
+//         if (token && userId) {
+//             // If a token exists, emit login event and navigate to chat
+//             socket.emit('login', userId);
+//             navigate('/chat', { state: { userId } }); // Redirect to chat
+
+//             // Update the user's location
+//             const updateLocation = async () => {
+//                 try {
+//                     await axios.post(`${baseURL}/api/auth/update-location`, { userId });
+//                 } catch (error) {
+//                     console.error('Error updating location:', error);
+//                 }
+//             };
+//             updateLocation();
+//         }
+//     }, [navigate]);
+
+//     const handleGoogleLoginSuccess = async (response) => {
+//         const id_token = response.credential;
+
+//         // Decode the id_token to get user information
+//         // const decodedToken = jwtDecode(id_token);
+//         // console.log("Decoded Token:", decodedToken);
+
+//         try {
+//             const res = await axios.post(`${baseURL}/api/auth/google`, { id_token });
+
+//             // alert(res.data.message);
+//             setGoogleUser(res.data.user); // Store Google user data
+
+//             // Check if additional information is needed
+//             if (!res.data.user.gender || !res.data.user.age || !res.data.user.location) {
+//                 setGoogleUser(res.data.user); // Store Google user data
+//             } else {
+//                 // If additional information is not needed, log in the user
+//                 onLogin(res.data.token); // Pass token to parent component
+//                 localStorage.setItem('token', res.data.token);
+//                 localStorage.setItem('userId', res.data.userId); // Store userId
+
+//                 // Emit login event with userId
+//                 socket.emit('login', res.data.userId);
+
+//                 navigate('/chat', { state: { userId: res.data.userId } }); // Navigate to chat after login
+//             }
+//         } catch (error) {
+//             // Display error message
+//             setErrorMessage(error.response?.data?.message || 'An error occurred. Please try again.');
+//         }
+//     };
+
+//     const handleGoogleLoginFailure = (error) => {
+//         setErrorMessage('Google login failed. Please try again.');
+//     };
+
+//     const handleAdditionalInfoSubmit = async (e) => {
+//         e.preventDefault();
+//         setErrorMessage(''); // Reset error message
+
+//         try {
+//             const response = await axios.post(`${baseURL}/api/auth/google/additional-info`, {
+//                 userId: googleUser._id,
+//                 gender,
+//                 age,
+//                 location,
+//             });
+//             // alert(response.data.message);
+//             onLogin(response.data.token); // Pass token to parent component
+//             localStorage.setItem('token', response.data.token);
+//             localStorage.setItem('userId', response.data.userId); // Store userId
+
+//             // Emit login event with userId
+//             socket.emit('login', response.data.userId);
+
+//             navigate('/chat', { state: { userId: response.data.userId } }); // Navigate to chat after login
+//         } catch (error) {
+//             // Display error message
+//             setErrorMessage(error.response?.data?.message || 'An error occurred. Please try again.');
+//         }
+//     };
+
+//     // Clean up on component unmount
+//     useEffect(() => {
+//         return () => {
+//             socket.disconnect();
+//         };
+//     }, []);
+
+//     return (
+//         <div className="login-container">
+//             {googleUser ? (
+//                 <form className="additional-info-form w-100" onSubmit={handleAdditionalInfoSubmit}>
+//                     <div className="form-group">
+//                         <h2>Additional Information</h2>
+//                         {errorMessage && <div className="alert alert-danger">{errorMessage}</div>}
+//                         <select
+//                             className="form-control"
+//                             aria-label="Gender"
+//                             value={gender}
+//                             onChange={(e) => setGender(e.target.value)}
+//                             required
+//                         >
+//                             <option value="" disabled>Select Gender</option>
+//                             <option value="male">Male</option>
+//                             <option value="female">Female</option>
+//                         </select>
+//                         <input
+//                             type="number"
+//                             className="form-control mt-3"
+//                             aria-label="Age"
+//                             placeholder="Age"
+//                             value={age}
+//                             onChange={(e) => setAge(e.target.value)}
+//                             required
+//                         />
+//                         {/* <select
+//                             className="form-control mt-3"
+//                             aria-label="Location"
+//                             value={location}
+//                             onChange={(e) => setLocation(e.target.value)}
+//                             required
+//                         >
+//                             <option value="" disabled>Select Location</option>
+//                             {provinces.map((province) => (
+//                                 <option key={province} value={province}>{province}</option>
+//                             ))}
+//                         </select> */}
+//                         <button type="submit" className="btn btn-primary mt-3">Submit</button>
+//                     </div>
+//                 </form>
+//             ) : (
+//                     <div className="google-login mx-auto">
+//                         <GoogleLogin
+//                             onSuccess={handleGoogleLoginSuccess}
+//                             onError={handleGoogleLoginFailure}
+//                             render={(renderProps) => (
+//                                 <button className="google-login-button" onClick={renderProps.onClick} disabled={renderProps.disabled}>
+//                                     <img src="https://developers.google.com/identity/images/g-logo.png" alt="Google logo" width="20" height="20" />
+//                                     Sign in with Google
+//                                 </button>
+//                             )}
+//                         />
+//                         {errorMessage && <div className="alert alert-danger mt-3">{errorMessage}</div>}
+//                     </div>
+//             )}
+//         </div>
+//     );
+// };
+
+// export default Login;
+
+
+
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { io } from 'socket.io-client';
 import { GoogleLogin } from '@react-oauth/google';
 import jwtDecode from 'jwt-decode';
-// import getLocation from '../../utils/getLocation';
-import "./Login.css";
+import './Login.css';
 const baseURL = process.env.REACT_APP_BASE_URL;
 
 const socket = io(baseURL); // Initialize the socket connection
-
-
 
 const Login = ({ onLogin }) => {
     const [gender, setGender] = useState('');
@@ -259,14 +434,9 @@ const Login = ({ onLogin }) => {
     const handleGoogleLoginSuccess = async (response) => {
         const id_token = response.credential;
 
-        // Decode the id_token to get user information
-        // const decodedToken = jwtDecode(id_token);
-        // console.log("Decoded Token:", decodedToken);
-
         try {
             const res = await axios.post(`${baseURL}/api/auth/google`, { id_token });
 
-            // alert(res.data.message);
             setGoogleUser(res.data.user); // Store Google user data
 
             // Check if additional information is needed
@@ -304,7 +474,6 @@ const Login = ({ onLogin }) => {
                 age,
                 location,
             });
-            // alert(response.data.message);
             onLogin(response.data.token); // Pass token to parent component
             localStorage.setItem('token', response.data.token);
             localStorage.setItem('userId', response.data.userId); // Store userId
@@ -353,18 +522,6 @@ const Login = ({ onLogin }) => {
                             onChange={(e) => setAge(e.target.value)}
                             required
                         />
-                        {/* <select
-                            className="form-control mt-3"
-                            aria-label="Location"
-                            value={location}
-                            onChange={(e) => setLocation(e.target.value)}
-                            required
-                        >
-                            <option value="" disabled>Select Location</option>
-                            {provinces.map((province) => (
-                                <option key={province} value={province}>{province}</option>
-                            ))}
-                        </select> */}
                         <button type="submit" className="btn btn-primary mt-3">Submit</button>
                     </div>
                 </form>
@@ -373,6 +530,12 @@ const Login = ({ onLogin }) => {
                     <GoogleLogin
                         onSuccess={handleGoogleLoginSuccess}
                         onError={handleGoogleLoginFailure}
+                        render={(renderProps) => (
+                            <button className="google-login-button" onClick={renderProps.onClick} disabled={renderProps.disabled}>
+                                <img src="https://developers.google.com/identity/images/g-logo.png" alt="Google logo" width="20" height="20" />
+                                Sign in with Google
+                            </button>
+                        )}
                     />
                     {errorMessage && <div className="alert alert-danger mt-3">{errorMessage}</div>}
                 </div>
